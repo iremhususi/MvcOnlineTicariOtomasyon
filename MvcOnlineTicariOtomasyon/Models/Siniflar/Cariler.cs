90479c1cdbfe0cd598c9e8590f88cedbeae55ab7
irehusu <irehusu@TRIST-LPF439AQY>
2023-12-18 20:57:00 +0300

    Add project files.

---
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/Cariler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class Cariler
+    {
+        [Key]
+        public int Cariid { get; set; }
+        [Column(TypeName = "Varchar")]
+        [StringLength(30)]
+        public string CariAd { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(30)]
+        public string CariSoyad { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(13)]
+        public string CariSehir { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(50)]
+        public string CariMail { get; set; }
+        public SatisHareket SatisHareket { get; set; }
+    }
+}
